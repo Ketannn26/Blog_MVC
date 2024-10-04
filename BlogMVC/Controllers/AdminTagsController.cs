43f0a768dc6ecc35ec034d9477c67e74b9c0889f
Ketannn26 <ketansagadhara26@gmail.com>
2024-10-04 11:34:36 +0530

    4/10

---
+++ BlogMVC/Controllers/AdminTagsController.cs
@@ -0,0 +1,44 @@
+﻿using BlogMVC.Data;
+using BlogMVC.Models.Domain;
+using BlogMVC.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogMVC.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private BlogDbContext blogdbContext;
+        public AdminTagsController(BlogDbContext blogDbContext) 
+        {
+          this.blogdbContext = blogDbContext;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest atr)
+        {
+            var tag = new Tag
+            {
+                Name = atr.Name,
+                DisplayName = atr.DisplayName,
+            };
+
+            blogdbContext.Add(tag);
+            blogdbContext.SaveChanges();
+            // return View("Add");
+            // return View("List");
+            return RedirectToAction("List");
+        }
+        [HttpGet]
+        public IActionResult List()
+        {
+            var tags = blogdbContext.Tags.ToList();
+            return View(tags);
+        }
+    }
+}
